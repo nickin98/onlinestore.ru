607b85d833b7b8c7e8ac97a7af6e50063ba3b84a
nickin98 <zlo-nic@mail.ru>
2019-07-23 22:03:55 +0500

    add comment and structure

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="css/style.css" type="text/css"/>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Site</title>
+	<meta name="keywords" content="keywords">
+    <meta name="description" content="description_site">
 </head>
 <body>
-	
+	<header>
+		
+	</header>
+	<nav>
+		
+	</nav>
+	<main>
+		<article>
+			
+		</article>
+		<article>
+			
+		</article>
+	</main>
+	<footer>
+		
+	</footer>
 </body>
 </html>
