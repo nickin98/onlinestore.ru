82956585997b251f9a004cd6b4f6e4cc4a3c4775
nickin98 <zlo-nic@mail.ru>
2019-07-28 20:31:52 +0500

    add layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,18 +9,77 @@
 </head>
 <body>
 	<header>
-		
+		<div id="navbar-social">
+			<p>Мы в соцсетях:</p>
+			<a href="#"><img src="img/vk_icon.ico"></a>
+			<a href="#"><img src="img/inst_icon.ico"></a>
+		</div>
+		<div id="navbar-logo">
+			<a href="#"><img src="img/clock_icon.ico"></a>
+			<p>Время работы</p>
+			<p>понедельник - воскресенье</p>
+			<p>с 10:00 - 02:00</p>
+			<p>Адрес: г.Верхняя Пышма Успенский проспект 56, паркорвка ТЦ Куприт</p>
+			<a href="#"><h1>DURUM</h1></a>
+			<a href="#"><img src="img/delivery_icon.png"></a>
+			<p>Доставка(подробнее)</p>
+			<p>+7(922)600-64-14</p>
+			<a href="#"><img src="img/buy_icon.png"></a>
+		</div>
 	</header>
 	<nav>
-		
+		<ul>
+			<li>Меню доставки</li>
+			<li>Акции</li>
+			<li>О компании</li>
+			<li>Вход/Регистрация</li>
+		</ul>
 	</nav>
 	<main>
-		<article>
-			
-		</article>
-		<article>
-			
-		</article>
+		<section>
+			<article>
+				<div class="name_product">
+					<a href="#"><h2>ДУРУМ(Шаурма по Армянски)</h2></a>
+				</div>
+				<div class="about_product">
+					<p>Свежие огурцы</p>
+					<img src="img/durum.jpg">
+					<section>
+						<p>Масса: 300г</p>
+						<p>120 Руб.</p>
+						<p>В корзину</p>
+					</section>
+				</div>
+			</article>
+			<article>
+				<div class="name_product">
+					<a href="#"><h2>ДУРУМ(Шаурма по Армянски)</h2></a>
+				</div>
+				<div class="about_product">
+					<p>Свежие огурцы</p>
+					<img src="img/durum.jpg">
+					<section>
+						<p>Масса: 300г</p>
+						<p>120 Руб.</p>
+						<p>В корзину</p>
+					</section>
+				</div>
+			</article>
+						<article>
+				<div class="name_product">
+					<a href="#"><h2>ДУРУМ(Шаурма по Армянски)</h2></a>
+				</div>
+				<div class="about_product">
+					<p>Свежие огурцы</p>
+					<img src="img/durum.jpg">
+					<section>
+						<p>Масса: 300г</p>
+						<p>120 Руб.</p>
+						<p>В корзину</p>
+					</section>
+				</div>
+			</article>
+		</section>
 	</main>
 	<footer>
 		
