2fee207853e9ad000c10765cd73392765006512d
Nirsen <nirsen@mail.ru>
2019-08-22 22:46:55 +0500

    Создал MenuController

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'MenuController@index')->name('menu');
