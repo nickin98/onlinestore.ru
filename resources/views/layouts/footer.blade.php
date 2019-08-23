df3cbbe306591928a613432b2e39845bb36faf31
nickin98 <zlo-nic@mail.ru>
2019-08-23 17:43:29 +0500

    Сделана витрина и футер

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="footer-copyright col-lg-6">
+                © 2019 “Алексей Конюхов и Никита Злобин
+            </div>
+            <div class="social-bar col-lg-6 text-right">
+                Социальные сети:&nbsp&nbsp&nbsp
+                <a href="#"><img src="images/inst_logo.png"> Instagram</a>
+                <a href="#"><img src="images/vk_logo.png"> Вконтакте</a>
+            </div>
+        </div>
+    </div>
+</footer>
