648444077842734aea52b8a781f76dae57062ae0
nickin98 <zlo-nic@mail.ru>
2019-08-24 18:04:17 +0500

    test

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -24,12 +24,30 @@
                 </li>
             </ul>
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Вход</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Регистрация</a>
-                </li>
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Войти</a></li>
+                    <li class="nav-item"><a class="nav-link"href="{{ route('register') }}">Регистрация</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Выход
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
