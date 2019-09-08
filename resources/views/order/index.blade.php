15457c28831b0daf54f72bd4488758f554eb5455
nickin98 <zlo-nic@mail.ru>
2019-09-08 13:33:55 +0500

    Добавлены таблицы заказов.

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,1 +1,61 @@
-<?php
+@extends('admin')
+
+@section('content')
+
+        <div class="adm-category">
+            <div class="name-category">
+                <div class="row">
+                    <h2 class="col-11">Товары</h2>
+                    <a src="#"><img src="/images/admin/add_icon.png"></a>
+                </div>
+            </div>
+
+        </div>
+        <div class="table-category">
+            <table class="table table-hover table-Light">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Категория</th>
+                    <th scope="col">Товар</th>
+                    <th scope="col">Номер телефона</th>
+                    <th scope="col">Действия</th>
+
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Шаурма</td>
+                    <td>Шаурма (сырная)</td>
+                    <td>+7(912)285-66-39</td>
+                    <td class="icon-table">
+                        <a src="#"><img src="/images/admin/see_icon.png"></a>
+                        <a src="#"><img src="/images/admin/complete_order.png"></a>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Шаурма</td>
+                    <td>Шаурма (Чесночная)</td>
+                    <td>+7(912)285-66-39</td>
+                    <td class="icon-table">
+                        <a src="#"><img src="/images/admin/see_icon.png"></a>
+                        <a src="#"><img src="/images/admin/complete_order.png"></a>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Вафля</td>
+                    <td>Вафля (армянская)</td>
+                    <td>+7(912)285-66-39</td>
+                    <td class="icon-table">
+                        <a src="#"><img src="/images/admin/see_icon.png"></a>
+                        <a src="#"><img src="/images/admin/complete_order.png"></a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    {{ $categories->links('vendor.pagination.bootstrap-4') }}
+@endsection
--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,1 +1,25 @@
-<?php
+@extends('admin')
+
+@section('content')
+    <div class="wrapper">
+        <h1>Заказ № </h1>
+        <table>
+            <tr>
+                <th>ФИО</th>
+            </tr>
+            <tr>
+                <th>Номер телефона:</th>
+            </tr>
+            <tr>
+                <th>Адрес:</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Категория</th>
+            </tr>
+            <tr>
+                <th>Заказ</th>
+            </tr>
+        </table>
+    </div>
+@endsection
